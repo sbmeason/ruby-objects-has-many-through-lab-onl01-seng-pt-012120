3d449ed14ae4ba663c522e735c9a79a1f0744e0a
Samantha Meason <sbmeason@gmail.com>
2020-02-23 23:30:42 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+class Appointment
+  attr_accessor :date
+  attr_reader :doctor, :patient
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+
+end
